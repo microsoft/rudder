d1dd9b1eeba83a30980818390f2eadfd467f83c9
Diego Garbervetsky <diego.garbervetsky@gmail.com>
2016-08-11 19:16:22 -0700

    Add new project for analyzing in a folder with many scope scripts

---
+++ ScopeProgramAnalysis/ScopeAnalysisBulkScripts/BulkAnalysis.cs
@@ -0,0 +1,47 @@
+﻿using ScopeProgramAnalysis;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopeAnalysisBulkScripts
+{
+    class BulkAnalysis
+    {
+        static void Main(string[] args)
+        {
+            AnalyzeScopeScripts(new string[] { @"D:\MadanExamples\", @"D:\Temp\", "Reducer" });
+            AnalysisStats.PrintStats(System.Console.Out);
+            System.Console.ReadKey();
+        }
+        public static void AnalyzeScopeScripts(string[] args)
+        {
+            var inputFolder = args[0];
+            var outputFolder = args[1];
+            var kind = args[2];
+            const string inputDllName = "__ScopeCodeGen__.dll";
+            string[] files = Directory.GetFiles(inputFolder, inputDllName, SearchOption.AllDirectories);
+            foreach (var dllToAnalyze in files)
+            {
+                System.Console.WriteLine("=========================================================================");
+                System.Console.WriteLine("Analyzing {0}", dllToAnalyze);
+                var folder = Path.GetDirectoryName(dllToAnalyze);
+                var referencesPath = Directory.GetFiles(folder, "*.dll", SearchOption.TopDirectoryOnly).Where(fp => Path.GetFileName(fp) != inputDllName).ToList();
+                referencesPath.AddRange(Directory.GetFiles(folder, "*.exe", SearchOption.TopDirectoryOnly));
+
+                string[] directories = folder.Split(Path.DirectorySeparatorChar);
+                var outputPath = Path.Combine(outputFolder, directories.Last()) + "_" + Path.ChangeExtension(Path.GetFileName(dllToAnalyze), ".sarif");
+
+                //var outputPath = Path.Combine(outputFolder, Path.ChangeExtension(Path.GetFileName(dllToAnalyze),".sarif"));
+
+                Program.AnalyzeDll(dllToAnalyze, referencesPath, outputPath, Program.ScopeMethodKind.Reducer);
+                System.Console.WriteLine("=========================================================================");
+            }
+            System.Console.WriteLine("Done!");
+        }
+
+
+    }
+}
---
+++ ScopeProgramAnalysis/ScopeAnalysisBulkScripts/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ScopeAnalysisBulkScripts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ScopeAnalysisBulkScripts")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf337a56-0ce5-43ea-92e3-270994332312")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
