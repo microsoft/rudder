3d65934d25eaacf0aaeb63c5bce74d1568c4a3f3
Zvonimir Pavlinovic <t-zpavli@microsoft.com>
2016-08-18 14:18:43 -0700

    Functionaliy for discovery of Scope-annotated reducers added, but not used.

---
+++ ColumnReductionAnalysis/BulkScopeAnalyzer/ScopeAnnotationAnalyzer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Cci;
+using Frontend;
+using Backend;
+
+namespace BulkScopeAnalyzer
+{
+    class ScopeAnnotationAnalyzer : MetadataTraverser
+    {
+        Assembly assembly;
+
+        public HashSet<string> UDFS { get; }
+
+
+        public ScopeAnnotationAnalyzer(Assembly asm)
+        {
+            assembly = asm;
+            UDFS = new HashSet<string>();
+        }
+
+
+        public void Analyze()
+        {
+            base.Traverse(assembly.Module);
+        }
+
+        public override void TraverseChildren(INamedTypeDefinition type)
+        {
+            if (type.Attributes.Count() > 1)
+            {
+                foreach (var atr in type.Attributes)
+                {
+                    // We look only for Scope annotation attributes.
+                    if (Microsoft.Cci.TypeHelper.GetTypeName(atr.Type, NameFormattingOptions.Signature) != "ScopeRuntime.ScopeAnnotationAttribute")
+                    {
+                        continue;
+                    }
+
+                    if (atr.Arguments.Count() < 2) continue;
+                   
+                    if (atr.Arguments.ElementAt(0) is Microsoft.Cci.IMetadataConstant)
+                    {
+                        var argName = (atr.Arguments.ElementAt(0) as Microsoft.Cci.IMetadataConstant).Value.ToString();
+                        if (argName == "OriginalClassName")
+                        {
+                            UDFS.Add((atr.Arguments.ElementAt(1) as Microsoft.Cci.IMetadataConstant).Value.ToString());
+                        }
+                    }                     
+                }
+            }
+
+            base.TraverseChildren(type);
+        }      
+    }
+}
