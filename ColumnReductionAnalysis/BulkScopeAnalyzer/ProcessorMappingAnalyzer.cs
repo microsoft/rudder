f19396573fd077ce1a5d663b137fe3990b64926a
Zvonimir Pavlinovic <t-zpavli@microsoft.com>
2016-08-19 11:26:18 -0700

    Added incomplete approach to disregarding compiler generatad processors.

--- ColumnReductionAnalysis/BulkScopeAnalyzer/ProcessorMappingAnalyzer.cs
+++ ColumnReductionAnalysis/BulkScopeAnalyzer/ProcessorMappingAnalyzer.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Microsoft.Cci;
 using Frontend;
 using Backend;
@@ -13,6 +12,9 @@ namespace BulkScopeAnalyzer
     {
         Assembly assembly;
         private Dictionary<string, string> mapping = new Dictionary<string, string>();
+
+        private Regex[] compilerGeneretedMethodMatchers = new Regex[] { new Regex(@"^___Scope_Generated_Classes___.ScopeFilterTransformer_\d+$", RegexOptions.Compiled),
+                                                                        new Regex(@"^___Scope_Generated_Classes___.ScopeGrouper_\d+$", RegexOptions.Compiled) };
 
         public ProcessorMappingAnalyzer(Assembly asm)
         {
@@ -29,8 +31,8 @@ namespace BulkScopeAnalyzer
             var name = Microsoft.Cci.TypeHelper.GetTypeName(methodDefinition.ContainingType,
                 Microsoft.Cci.NameFormattingOptions.OmitContainingNamespace);
 
-          if (name == "___Scope_Generated_Classes___.__OperatorFactory__")
-          {
+            if (name == "___Scope_Generated_Classes___.__OperatorFactory__")
+            {
                 var mname = methodDefinition.Name.Value;
                 if (mname.StartsWith("Create_"))
                 {
@@ -38,14 +40,15 @@ namespace BulkScopeAnalyzer
                     {
                         var id = "Process_" + mname.Split('_').Last();
                         var processorName = GetProcessorName(methodDefinition);
-                        mapping[processorName] = id;
+                        if (processorName != null)
+                            mapping[processorName] = id;
                     }
                     catch
                     {
                         Console.WriteLine("ERROR: failed to extract specific processor id: " + mname);
                     }
                 }
-          }
+            }
         }
 
 
@@ -55,8 +58,24 @@ namespace BulkScopeAnalyzer
             var methodBody = disassembler.Execute();
             var ins = methodBody.Instructions.OfType<Backend.ThreeAddressCode.Instructions.CreateObjectInstruction>().Single();
             var ct = ins.Constructor.ContainingType;
-            return Microsoft.Cci.TypeHelper.GetTypeName(ct, NameFormattingOptions.Signature);
+            var name =  Microsoft.Cci.TypeHelper.GetTypeName(ct, NameFormattingOptions.Signature);
+
+            if (IsCompilerGenerated(name))
+                return null;
+
+            return name;
         }
+
+        private bool IsCompilerGenerated(string name)
+        {
+            foreach(var reg in compilerGeneretedMethodMatchers)
+            {
+                if (reg.IsMatch(name.Trim()))
+                    return true;
+            }
+            return false;
+        }
+
 
         public void Analyze()
         {
