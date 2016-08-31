5d6cee3c8e4de7019953116f687ed13058db6b07
Mike Barnett <mbarnett@microsoft.com>
2016-08-31 14:04:06 -0700

    Add schema for unit tests (and the ability to process a schema from such a source).

--- ScopeProgramAnalysis/Tests/CodeUnderTest/Class1.cs
+++ ScopeProgramAnalysis/Tests/CodeUnderTest/Class1.cs
@@ -67,26 +67,4 @@ namespace CodeUnderTest
         }
     }
 
-    public class ProjectOnlyOneColumnProcessor : Processor
-    {
-        public override Schema Produces(string[] requested_columns, string[] args, Schema input_schema)
-        {
-            var output_schema = input_schema.Clone();
-            var newcol = new ColumnInfo("NewColumn", typeof(string));
-            output_schema.Add(newcol);
-            return output_schema;
-        }
-
-        public override IEnumerable<Row> Process(RowSet input_rowset, Row output_row, string[] args)
-        {
-            foreach (Row input_row in input_rowset.Rows)
-            {
-                input_row.CopyTo(output_row);
-                string market = input_row[0].String;
-                output_row[2].Set("FOO" + market);
-                yield return output_row;
-            }
-        }
-    }
-
 }
